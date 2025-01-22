d1456f390d0bf78864d69224cb6d98c278541a5b
mahesh3267 <mahesh3267@gmail.com>
2025-01-22 20:39:46 +0000

    Update App.java

--- src/main/java/com/itspark/App.java
+++ src/main/java/com/itspark/App.java
@@ -5,6 +5,6 @@ package com.itspark;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! 123");
+        System.out.println("Hello World!");
     }
 }
